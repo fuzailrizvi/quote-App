05c709d37aa1182082c52a30391bb83e60907ee2
“Fuzail <fuzailrizvi786@gmail.com>
2024-11-12 18:11:15 +0530

    frontend start 1

---
+++ backend/seed.js
@@ -0,0 +1,22 @@
+const randomQuotesGenerator=()=>{
+    const randomQuotes=[
+        "The best way to predict the future is to create it.",
+        "Success is not final, failure is not fatal: It is the courage to continue that counts.",
+        "It does not matter how slowly you go as long as you do not stop.",
+        "In the end, we will remember not the words of our enemies, but the silence of our friends.",
+        "To be yourself in a world that is constantly trying to make you something else is the greatest accomplishment.",
+        "It always seems impossible until it's done.",
+        "You are never too old to set another goal or to dream a new dream.",
+        "Don't watch the clock; do what it does. Keep going.",
+        "Success is not in what you have, but who you are.",
+        "The only way to do great work is to love what you do.",
+    ];
+
+    return randomQuotes[Math.random(Math.random()*randomQuotes.length)];
+    
+}
+
+const seedDB=()=>{
+    
+}
+
