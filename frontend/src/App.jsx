150b5f3200b29c516f70894a1fa40e40d3e5c3c2
“Fuzail <fuzailrizvi786@gmail.com>
2024-12-11 17:23:49 +0530

    Login.jsx completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,26 +1,36 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Navbar from './components/Navbar'
 import Home from './pages/Home'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes, useNavigate } from 'react-router-dom'
 import Login from './pages/Login'
 import Register from './pages/Register'
 
 const App = () => {
+  const [isAuthenticated,setIsAuthenticated]=useState("");
+  const navigate=useNavigate();
+  const user=localStorage.getItem('user');
+
+  useEffect(()=>{
+    setIsAuthenticated(localStorage.getItem('token'))
+  },[isAuthenticated])
   
   function handleLogin(id,email,fullName,token){
       localStorage.setItem('token',token);
       localStorage.setItem('user',JSON.stringify({id,email,fullName}));
+      setIsAuthenticated(true);
+      
+      navigate("/");
       
   }
   
   
   return (
-    <div>
+    <div className='max-h-screen min-h-screen dark:bg-slate-800'>
       
       <Navbar/>
       <Routes>
         <Route path={'/'} element={<Home/>}/>
-        <Route path={'/login'} element={<Login handleLogin={handleLogin}/>}/>
+        <Route path={'/login'} element={<Login isAuthenticated={isAuthenticated} handleLogin={handleLogin}/>}/>
         <Route path={'/register'} element={<Register/>}/>
       </Routes>
      
--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,10 +1,12 @@
 import React, { useState } from 'react'
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import  axios  from "axios";
-const Login = ({handleLogin}) => {
+import { useEffect } from 'react';
+const Login = ({handleLogin,isAuthenticated}) => {
   const[email,setEmail]=useState("");
   const[password,setPassword]=useState("");
-  
+  const navigate=useNavigate();
+
   async function submitHandler(){
    try {
     const res=await axios.post(`${import.meta.env.VITE_BACKEND_URL}/api/v1/auth/login`,{
@@ -25,6 +27,12 @@ const Login = ({handleLogin}) => {
    
     
   }
+
+  useEffect(()=>{
+    if(isAuthenticated){
+      navigate("/");
+    }
+  })
   
   return (
     <div>
